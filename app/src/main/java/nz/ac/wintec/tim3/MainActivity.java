f1448df11d45604633fd82157ceb4edc6f0661d0
timhot <timhot@hotmail.com>
2016-08-27 17:44:52 +1200

    modify main activity added test.java

--- app/src/main/java/nz/ac/wintec/tim3/MainActivity.java
+++ app/src/main/java/nz/ac/wintec/tim3/MainActivity.java
@@ -51,6 +51,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void testAdd(){
-        int i = 1 +1;
+        int i = 1 +1+1;
     }
 }
---
+++ app/src/main/java/nz/ac/wintec/tim3/Test.java
@@ -0,0 +1,10 @@
+package nz.ac.wintec.tim3;
+
+/**
+ * Created by User on 27-Aug-16.
+ */
+public class Test {
+    public void myMethod(){
+        int i = 2;
+    }
+}
